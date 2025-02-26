a6adf9dbd7a53f64a85504309994921a838627ef
Moodstoryvn <hoang.editor2ka10@gmail.com>
2025-02-26 13:54:39 +0700

    Hoangnguyen.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.tiktok.com/@editcapcut_vn?_t=ZS-8uEPAMGliN8&_r=1' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
